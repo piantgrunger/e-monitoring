7df57f5c7b11cd55be8457143792b71285b31582
piant <piant.grunger@gmail.com>
2021-11-30 15:24:22 +0700

    first commit

---
+++ config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return [
+    'adminEmail' => 'admin@example.com',
+     'ppn' =>10,
+
+	'supportEmail' => 'support@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+];
---
+++ views/site/index.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\helpers\Html;
+use hscstudio\mimin\components\Mimin;
+
+// $this->registerCSSFile('css/metro-all.css');
+// $this->registerCSSFile('css/start.css');
+// $this->registerJSFile(Yii::$app->homeUrl.'js/metro.min.js', ['depends' => [yii\web\JqueryAsset::className()]]);
+// $this->registerJSFile(Yii::$app->homeUrl.'js/start.js', ['depends' => [yii\web\JqueryAsset::className()]]);
+/* @var $this yii\web\View */
+/* @var $this yii\web\View */
+
+?>
+    <div class="container-fluid start-screen no-scroll-y h-100">
+
+
+
+</div>
